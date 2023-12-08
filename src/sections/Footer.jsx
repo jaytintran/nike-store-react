c91e1159ebe02506e73130c6aa648190f3b9f24f
Tin Tran <jaytintran@gmail.com>
2023-12-09 00:15:29 +0700

    start the footer structure and style

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,34 @@
+import { footerLogo } from "../assets/images"
+import { socialMedia } from "../constants"
+
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className="max-container text-white">
+			<div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+				<div className="flex flex-col items-start">
+					<a href="/">
+						<img src={footerLogo} height={150} width={46} />
+					</a>
+
+					<p className="mt-6 text-base leading-7 font-monserrat text-white-400">
+						Get your newest design shoe in the nearest store now. To get a
+						perfect size and extra rewards.
+					</p>
+				</div>
+			</div>
+
+			<div>
+				{socialMedia.map(
+					(social) =>
+						(
+							<div key={social.alt}>
+								<img src={social.src} alt={social.alt} className="fill-white" />
+							</div>
+						)``
+				)}
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
